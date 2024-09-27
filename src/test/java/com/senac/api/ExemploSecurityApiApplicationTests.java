6a9ddaa0e72353448e7ea1b7de32bc50e39f2b8f
CaduhPavei <CaduhPavei@gmail.com>
2024-09-27 19:46:38 -0300

    Aparentemente, tá rodando...

--- src/test/java/com/senac/api/ExemploSecurityApiApplicationTests.java
+++ src/test/java/com/senac/api/ExemploSecurityApiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.senac.api;
 
+import com.senac.api.useCases.users.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ExemploSecurityApiApplicationTests {
 
+	@Autowired
+	private UserService userService;
+
 	@Test
 	void contextLoads() {
 	}
+
+//	@Test
+//	void userRegistryTest(){
+//		UserRequestDom userPersist = new UserRequestDom();
+//		userPersist.setLogin("test");
+//		userPersist.setPassword("1234");
+//
+//		UserResponseDom userResult = userService.createUser(userPersist);
+//	}
 
 }
